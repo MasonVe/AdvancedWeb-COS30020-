f276f3d6842330bbeed6cc97fdf3da7301945cab
MasonVe <61397163+MasonVe@users.noreply.github.com>
2024-07-22 10:32:30 +1000

    Week 1 Tasks Submission

---
+++ Week 1/cdrates.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title> Task 3 </title>
+	<meta charset="utf-8">
+	<meta name="description" content="Web Development">
+	<meta name="keywords" content="HTML, CSS, JavaScript">
+	<meta name="author" content="Mason Veljan">
+</head>
+
+<body>
+	<?php
+		echo "<h1>Forestville Credit Union</h1>";
+		echo "<h2>CD Rates</h2>";
+		echo "<ul>";
+			echo "<li>4.35% (36-Month Term CD)</li>";
+			echo "<li>3.85% (12-Month Term CD)</li>";
+			echo "<li>2.65% (6-Month Term CD)</li>";
+		echo "</ul>";
+		echo "<p>$1,000 minimum Deposit</p>";
+		# Mason Veljan (101722004)
+		# 11/07/2024
+	?>
+
+</body>
+</html>
